1b4936b11b3a95c9013e603750ee6c87391106b5
kejedi <kejedi@icloud.com>
2024-08-06 04:21:36 -0400

    rm id & timestamps from pivot

--- stubs/Pivot.php
+++ stubs/Pivot.php
@@ -2,17 +2,14 @@
 
 namespace DummyNamespace;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 use Kejedi\Lucid\Database\LucidBlueprint;
 
 class DummyClass extends Pivot
 {
-    use HasUuids;
-
     public function lucidSchema(LucidBlueprint $table): void
     {
-        $table->uuid('id')->primary();
-        $table->timestamps();
+        $table->uuid('first_id')->index();
+        $table->uuid('second_id')->index();
     }
 }
